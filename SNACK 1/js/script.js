0ef00f654ba62ae638fdc1e2219fbd579712b438
mariaDulesova <maria.dulesova@gmail.com>
2021-05-11 13:36:45 +0200

    Snack 1

---
+++ SNACK 1/js/script.js
@@ -0,0 +1,39 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: name e peso.
+// Stampare a schermo la bici con peso minore.
+
+var bike = [
+    {
+        name: "bici1",
+        weight: 8.4,
+    },
+    {
+        name: "bici2",
+        weight: 10,
+    },
+    {
+        name: "bici3",
+        weight: 9,
+    },
+    {
+        name: "bici4",
+        weight: 4.3,
+    },
+    {
+        name: "bici5",
+        weight: 2.5,
+    },
+
+]
+
+
+var bikeLight = bike[0].weight;
+
+for (var i=1; i<bike.length; i++){
+    if(!(bikeLight < bike[i].weight)){
+        bikeLight = bike[i].weight;
+    }  
+}
+console.log(bikeLight);
+
